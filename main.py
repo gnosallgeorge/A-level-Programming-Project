7566f369bf44f7eeaf2460c1aefcc9a01759f7b8
gnosallgeorge <61749725+gnosallgeorge@users.noreply.github.com>
2024-05-02 13:30:34 +0100

    vectorisation
    changed all position and velocity data to numpy vectors. Altered some background code to make it more reliable. Also installed numpy.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame, sys, random
 from pygame.locals import QUIT
 from PIL import Image, ImageOps
+import numpy as np
 
 pygame.init()
 scale = 720/1080
@@ -11,58 +12,60 @@ frame_time = 1/framerate
 clock = pygame.time.Clock()
 
 class background:
-  
-  
   def __init__(self):
-    self.width = int(1024*scale)
-    self.height = int(1024*scale)
     background_PIL = Image.open("Images/background.png")
+    self.image_size = np.array(background_PIL.size)
+    self.scaled_image_size = np.trunc(self.image_size*scale)
+    self.width = int(self.scaled_image_size[0])
+    self.height = int(self.scaled_image_size[1])
     background_PIL = ImageOps.fit(background_PIL, (self.width,self.height)).save("converted_background.png")
     print(background_PIL)
     self.background_image = pygame.image.load("converted_background.png")
   def draw(self):
     player_pos = player.get_position()
+    print(player_pos)
     # converts the player coordinates to snap coordinates for the map image
-    player_grid_pos = (int(player_pos[0]//self.width),int(player_pos[1]//self.height))
+    player_grid_pos = np.trunc(player_pos/self.image_size)
+    print(player_grid_pos)
     # blits map images in a 3x3 grid where the player is in the centre tile
     for x in range(-1,2):
       for y in range(-1,2):
-        DISPLAYSURF.blit(self.background_image, ((player_grid_pos[0]+x)*self.width,(player_grid_pos[1]+y)*1024))
+        offset = np.array((x,y))
+        DISPLAYSURF.blit(self.background_image, ((player_grid_pos+offset)*self.scaled_image_size))
 
 background = background()
 # Entity class is used for players, enemies and projectiles and anything else that can collide with other entities or move around the screen
 
 class entity:
   def __init__(self, radius, position):
-    self.position = position
+    self.position = np.array(position)
     self.radius = radius
-    self.velocity = [0,0]
+    self.velocity = np.array([0,0])
   def set_position(self, new_position):
-    self.position = new_position
+    self.position = np.array(new_position)
   def get_position(self):
     return self.position
   # velocity is set as a list [velocity_x, velocity_y]
   def set_velocity(self, velocity):
-    self.velocity = velocity
+    self.velocity = np.array(velocity)
   def set_x_velocity(self, x_velocity):
     self.velocity[0] = x_velocity
   def set_y_velocity(self, y_velocity):
     self.velocity[1] = y_velocity
   def move(self):
-    new_pos = (self.position[0]+self.velocity[0]*frame_time,self.position[1]+self.velocity[1]*frame_time)
-    self.set_position(new_pos)
+    self.position = self.position+self.velocity*frame_time
   # this will draw any entities onto the screen. currently defined as a blue circle
   def draw(self):
       self.move()
-      pygame.draw.circle(DISPLAYSURF,(0,0,255),self.position,self.radius)
+      pygame.draw.circle(DISPLAYSURF,(0,0,255),self.position*scale,int(self.radius*scale))
 
 # player specific code such as weapons, exp and health will stay in this class
 class player(entity):
   def __init__(self, radius, position):
     super().__init__(radius, position)
   
-player = player(100, [500,500])
-player.set_y_velocity((100))  
+player = player(100, [450,200])
+player.set_x_velocity((100))  
 
 
 
